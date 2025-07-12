811234d780d1ede28b613baa01fa28013409d7d3
Quan Nguyen <vozermuonnam1@gmail.com>
2025-07-12 18:01:06 +0700

    fix

--- src/main/java/com/example/FirestoreInit.java
+++ src/main/java/com/example/FirestoreInit.java
@@ -6,12 +6,21 @@ import com.google.firebase.FirebaseOptions;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.cloud.FirestoreClient;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class FirestoreInit {
     public static Firestore initialize() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("firebase-service-account.json");
+        String jsonKey = System.getenv("FIREBASE_CREDENTIALS_JSON");
+        if (jsonKey == null) {
+            throw new RuntimeException("FIREBASE_CREDENTIALS_JSON env var is missing.");
+        }
+
+        InputStream serviceAccount = new ByteArrayInputStream(jsonKey.getBytes(StandardCharsets.UTF_8));
+
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
