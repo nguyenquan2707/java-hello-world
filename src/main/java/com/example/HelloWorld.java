0fd51bc67f12c5bc2d942976263354c336423246
Quan Nguyen <vozermuonnam1@gmail.com>
2025-07-12 17:19:23 +0700

    add loop

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,7 +1,12 @@
 package com.example;
 
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("🚀 Hello from Java - deployed in the cloud!");
+
+        // Keep app alive
+        while (true) {
+            Thread.sleep(1000); // Sleep 1 giây
+        }
     }
 }
